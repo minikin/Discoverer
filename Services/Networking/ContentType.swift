b0158512773d1bccc627942ff23ea38f6ffd801c
Sasha Prokhorenko <djminikin@gmail.com>
2019-06-24 14:09:21 +0200

    Add ContentType

---
+++ Services/Networking/ContentType.swift
@@ -0,0 +1,14 @@
+//
+//  ContentType.swift
+//  Discoverer
+//
+//  Created by Developer on 24.06.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import Foundation
+
+enum ContentType: String {
+	case json = "application/json"
+	case xml = "application/xml"
+}
