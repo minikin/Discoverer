28d0d3111aacc0f42fc765ce9d5ccc7ee4981561
Sasha Prokhorenko <djminikin@gmail.com>
2019-06-24 14:26:02 +0200

    Add Resource

---
+++ Services/Networking/Resource.swift
@@ -0,0 +1,34 @@
+//
+//  Resource.swift
+//  Discoverer
+//
+//  Created by Developer on 24.06.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+final class Resource<A>: BindableObject {
+    let didChange = PassthroughSubject<A?, Never>()
+    let endpoint: Endpoint<A>
+    var value: A? {
+        didSet {
+            sleep(2)
+            DispatchQueue.main.async {
+                self.didChange.send(self.value)
+            }
+        }
+    }
+
+    init(endpoint: Endpoint<A>) {
+        self.endpoint = endpoint
+        reload()
+    }
+
+    func reload() {
+        URLSession.shared.load(endpoint) { result in
+            self.value = try? result.get()
+        }
+    }
+}
