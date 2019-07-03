f7a6be8fd5478552c65044fb9205012eae611f77
Sasha Prokhorenko <djminikin@gmail.com>
2019-07-03 10:46:56 +0200

    Add gitignore

--- Discoverer/App/Views/SignInForm.swift
+++ Discoverer/App/Views/SignInForm.swift
@@ -8,7 +8,8 @@
 
 import SwiftUI
 
-func signInForm(_ nickName: Binding<String>, _ password: Binding<String>) -> some View {
+func signInForm(_ nickName: Binding<String>,
+								_ password: Binding<String>) -> some View {
     VStack {
         textInputField(nickName,
                        placeholderText: "Nickname",
